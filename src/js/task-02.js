e5b73bdcc25ee2aed5fc63ca8dba6517765efc03
VladDyadenko <Stilmagroup@gmail.com>
2022-12-05 15:49:30 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const addIngredientsToWeb = ingredients.map(Array => {
+  let listEl = document.createElement("li");
+  listEl.textContent = Array;
+  listEl.classList = "item";
+  const itemEl = document.querySelector("#ingredients");
+  return itemEl.insertAdjacentElement("beforeend", listEl);
+
+});
